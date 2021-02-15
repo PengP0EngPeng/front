4b257bcb1db829019e5b1ddf15ee7ed4221645a0
xpb <562709540@qq.com>
2021-02-15 11:16:03 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app').use(ElementPlus)
