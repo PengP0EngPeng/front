67eca5f0fea94241028f1242fe7d253bc20744aa
xpb <562709540@qq.com>
2021-02-23 23:31:53 +0800

    根路径修改

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
     port: 8080,            //配置本项目运行端口
     proxy: {                //配置代理服务器来解决跨域问题
       '/api': {
-        target: 'http://aerowang.cn:6688/api/private/v1/',      //配置要替换的后台接口地址
+        target: 'http://aerowang.cn:6688/api/private/v1',      //配置要替换的后台接口地址
         changOrigin: true,                    //配置允许改变Origin
         pathRewrite: {
           '^/api': ''
